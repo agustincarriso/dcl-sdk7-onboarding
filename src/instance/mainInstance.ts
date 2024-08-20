fa1e14ca7523b9ca04e48a65df5dd99c281788c0
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2024-08-20 12:25:49 -0300

    New entities

--- src/instance/mainInstance.ts
+++ src/instance/mainInstance.ts
@@ -1,5 +1,5 @@
-import { engine, Entity, GltfContainer, Transform } from '@dcl/sdk/ecs'
-import { Vector3, Quaternion } from '@dcl/sdk/math'
+import { Animator, engine, Entity, GltfContainer, Material, MeshCollider, MeshRenderer, Transform } from '@dcl/sdk/ecs'
+import { Vector3, Quaternion, Color4 } from '@dcl/sdk/math'
 import { GameController } from '../controllers/gameController'
 
 export class MainInstace {
@@ -19,6 +19,36 @@ export class MainInstace {
   s0_Leaf_Anim_Art_01: Entity
   s0_Leaf_Anim_02_Art_1__01: Entity
   s0_Baliza_on_art_01: Entity
+  s0_Baliza_off_art_01: Entity
+  s0_Cables_Art_01: Entity
+  s0_Cable_01_OFF_01: Entity
+  s0_Cable_01_ON_01: Entity
+  s0_Cable_02_OFF_01: Entity
+  s0_Cable_02_ON_01: Entity
+  s0_Cable_03_OFF_01: Entity
+  s0_Cable_03_ON_01: Entity
+  s0_Cable_04_OFF_01: Entity
+  s0_Cable_04_ON_01: Entity
+  s0_NPC_Robot_Art_1__01: Entity
+  s0_Puzlemachines_01: Entity
+  s0_Cables_01: Entity
+  s0_Puzle_machines_01: Entity
+  s0_Sign_01_OFF_01: Entity
+  s0_Sign_01_ON_01: Entity
+  s0_Sign_02_OFF_01: Entity
+  // Entities
+  s0_Sign_02_ON_01: Entity
+  s0_Sign_03_OFF_01: Entity
+  s0_Sign_03_ON_01: Entity
+  s0_tube_01_01: Entity
+  s0_tube_01_OFF_01: Entity
+  s0_tube_01_ON_01: Entity
+  s0_tube_02_01: Entity
+  s0_tube_02_OFF_01: Entity
+  s0_tube_02_ON_01: Entity
+  s0_tube_03_01: Entity
+  s0_tube_03_OFF_01: Entity
+  s0_tube_03_ON_01: Entity
 
   gameController: GameController
   constructor(gameController: GameController) {
@@ -190,5 +220,393 @@ export class MainInstace {
       rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
       scale: Vector3.create(14, 14, 14)
     })
+
+    // Baliza_off_art
+    this.s0_Baliza_off_art_01 = engine.addEntity()
+    GltfContainer.create(this.s0_Baliza_off_art_01, {
+      src: 'assets/scene/models/unity_assets/s0_Baliza_off_art_01.glb'
+    })
+    Transform.create(this.s0_Baliza_off_art_01, {
+      position: Vector3.create(219.9794, 18.14, 127.1204),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(14, 14, 14)
+    })
+
+    // Cables_Art
+    this.s0_Cables_Art_01 = engine.addEntity()
+    Transform.create(this.s0_Cables_Art_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 0, 0, 1),
+      scale: Vector3.create(1, 1, 1)
+    })
+
+    // Cable_01_OFF
+    this.s0_Cable_01_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_01_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_01_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_01_OFF_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_01_OFF_01).parent = this.s0_Cables_Art_01
+
+    // Cable_01_ON
+    this.s0_Cable_01_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_01_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_01_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_01_ON_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_01_ON_01).parent = this.s0_Cables_Art_01
+
+    // Cable_02_OFF
+    this.s0_Cable_02_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_02_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_02_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_02_OFF_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_02_OFF_01).parent = this.s0_Cables_Art_01
+
+    // Cable_02_ON
+    this.s0_Cable_02_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_02_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_02_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_02_ON_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_02_ON_01).parent = this.s0_Cables_Art_01
+
+    // Cable_03_OFF
+    this.s0_Cable_03_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_03_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_03_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_03_OFF_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_03_OFF_01).parent = this.s0_Cables_Art_01
+
+    // Cable_03_ON
+    this.s0_Cable_03_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_03_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_03_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_03_ON_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_03_ON_01).parent = this.s0_Cables_Art_01
+
+    // Cable_04_OFF
+    this.s0_Cable_04_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_04_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_04_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_04_OFF_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_04_OFF_01).parent = this.s0_Cables_Art_01
+
+    // Cable_04_ON
+    this.s0_Cable_04_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Cable_04_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cable_04_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cable_04_ON_01.glb',
+      visibleMeshesCollisionMask: 0 // Disable collisions
+    })
+    Transform.getMutable(this.s0_Cable_04_ON_01).parent = this.s0_Cables_Art_01
+
+    //NPC_Robot_Art (1)
+    this.s0_NPC_Robot_Art_1__01 = engine.addEntity()
+    Transform.create(this.s0_NPC_Robot_Art_1__01, {
+      position: Vector3.create(218.95, 68.67, 127.08),
+      rotation: Quaternion.create(0, 0.5733939, 0, -0.8192798),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_NPC_Robot_Art_1__01, {
+      src: 'assets/scene/models/unity_assets/s0_NPC_Robot_Art_1__01.glb'
+    })
+    Animator.create(this.s0_NPC_Robot_Art_1__01, {
+      states: [
+        {
+          clip: 'Robot_On'
+        },
+        {
+          clip: 'Robot_off'
+        },
+        {
+          clip: 'Walk_End'
+        },
+        {
+          clip: 'Walk_Loop'
+        },
+        {
+          clip: 'Walk_Start'
+        },
+        {
+          clip: 'Robot_Idle'
+        },
+        {
+          clip: 'Talk'
+        }
+      ]
+    })
+    //Puzle machines
+    this.s0_Puzlemachines_01 = engine.addEntity()
+    Transform.create(this.s0_Puzlemachines_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 0, 0, 1),
+      scale: Vector3.create(1, 1, 1)
+    })
+    //Cables
+    this.s0_Cables_01 = engine.addEntity()
+    Transform.create(this.s0_Cables_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Cables_01, {
+      src: 'assets/scene/models/unity_assets/s0_Cables_01.glb'
+    })
+    Transform.getMutable(this.s0_Cables_01).parent = this.s0_Puzlemachines_01
+    // Puzle_machines
+    this.s0_Puzle_machines_01 = engine.addEntity()
+    Transform.create(this.s0_Puzle_machines_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Puzle_machines_01, {
+      src: 'assets/scene/models/unity_assets/s0_Puzle_machines_01.glb'
+    })
+    Transform.getMutable(this.s0_Puzle_machines_01).parent = this.s0_Puzlemachines_01
+
+    // Sign_01_OFF
+    this.s0_Sign_01_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Sign_01_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Sign_01_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Sign_01_OFF_01.glb'
+    })
+    Transform.getMutable(this.s0_Sign_01_OFF_01).parent = this.s0_Puzlemachines_01
+
+    // Sign_01_ON
+    this.s0_Sign_01_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Sign_01_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Sign_01_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Sign_01_ON_01.glb'
+    })
+    Transform.getMutable(this.s0_Sign_01_ON_01).parent = this.s0_Puzlemachines_01
+
+    // Sign_02_OFF
+    this.s0_Sign_02_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Sign_02_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Sign_02_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Sign_02_OFF_01.glb'
+    })
+    Transform.getMutable(this.s0_Sign_02_OFF_01).parent = this.s0_Puzlemachines_01
+
+    // Sign_02_ON
+    this.s0_Sign_02_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Sign_02_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Sign_02_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Sign_02_ON_01.glb'
+    })
+    Transform.getMutable(this.s0_Sign_02_ON_01).parent = this.s0_Puzlemachines_01
+
+    // Sign_03_OFF
+    this.s0_Sign_03_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_Sign_03_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Sign_03_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_Sign_03_OFF_01.glb'
+    })
+    Transform.getMutable(this.s0_Sign_03_OFF_01).parent = this.s0_Puzlemachines_01
+
+    // Sign_03_ON
+    this.s0_Sign_03_ON_01 = engine.addEntity()
+    Transform.create(this.s0_Sign_03_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_Sign_03_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_Sign_03_ON_01.glb'
+    })
+    Transform.getMutable(this.s0_Sign_03_ON_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_01
+    this.s0_tube_01_01 = engine.addEntity()
+    Transform.create(this.s0_tube_01_01, {
+      position: Vector3.create(98.704, 78.82, 140.141),
+      rotation: Quaternion.create(0, 0.9999142, 0, -0.01309833),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_01_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_01_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_01_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_01_OFF
+    this.s0_tube_01_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_tube_01_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_01_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_01_OFF_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_01_OFF_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_01_ON
+    this.s0_tube_01_ON_01 = engine.addEntity()
+    Transform.create(this.s0_tube_01_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_01_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_01_ON_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_01_ON_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_02
+    this.s0_tube_02_01 = engine.addEntity()
+    Transform.create(this.s0_tube_02_01, {
+      position: Vector3.create(98.704, 78.82, 140.141),
+      rotation: Quaternion.create(0, 0.9999142, 0, -0.01309833),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_02_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_02_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_02_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_02_OFF
+    this.s0_tube_02_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_tube_02_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_02_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_02_OFF_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_02_OFF_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_02_ON
+    this.s0_tube_02_ON_01 = engine.addEntity()
+    Transform.create(this.s0_tube_02_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_02_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_02_ON_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_02_ON_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_03
+    this.s0_tube_03_01 = engine.addEntity()
+    Transform.create(this.s0_tube_03_01, {
+      position: Vector3.create(98.704, 78.82, 140.141),
+      rotation: Quaternion.create(0, 0.9999142, 0, -0.01309833),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_03_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_03_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_03_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_03_OFF
+    this.s0_tube_03_OFF_01 = engine.addEntity()
+    Transform.create(this.s0_tube_03_OFF_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_03_OFF_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_03_OFF_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_03_OFF_01).parent = this.s0_Puzlemachines_01
+
+    // Tube_03_ON
+    this.s0_tube_03_ON_01 = engine.addEntity()
+    Transform.create(this.s0_tube_03_ON_01, {
+      position: Vector3.create(0, 0, 0),
+      rotation: Quaternion.create(0, 1, 0, -4.371139e-8),
+      scale: Vector3.create(1, 1, 1)
+    })
+    GltfContainer.create(this.s0_tube_03_ON_01, {
+      src: 'assets/scene/models/unity_assets/s0_tube_03_ON_01.glb'
+    })
+    Transform.getMutable(this.s0_tube_03_ON_01).parent = this.s0_Puzlemachines_01
+    // En_DanceZone
+    // this.s0_En_DanceZone_01 = engine.addEntity()
+    // Transform.create(this.s0_En_DanceZone_01, {
+    //   position: Vector3.create(163.995, 65.3468, 106.1981),
+    //   rotation: Quaternion.create(0, 0.8192798, 0, 0.5733939),
+    //   scale: Vector3.create(1, 1, 1)
+    // })
+    // // En_Npc1
+    // this.s0_En_Npc1_01 = engine.addEntity()
+    // MeshCollider.setCylinder(this.s0_En_Npc1_01)
+    // Transform.create(this.s0_En_Npc1_01, {
+    //   position: Vector3.create(160.0233, 66.64076, 104.139),
+    //   rotation: Quaternion.create(0, 0.3281288, 0, 0.944633),
+    //   scale: Vector3.create(0.7227439, 1.299883, 0.7050315)
+    // })
+    // Material.setPbrMaterial(this.s0_En_Npc1_01, {
+    //   albedoColor: Color4.create(1, 1, 1, 0),
+    //   metallic: 0,
+    //   roughness: 0.5
+    // })
   }
 }
