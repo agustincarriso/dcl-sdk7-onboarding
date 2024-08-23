a0263716c108e5bea572ee09f9cd6c12f47da1e2
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2024-08-23 13:13:34 -0300

    Keyboard UI

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -5,25 +5,24 @@ import { Vector3 } from '@dcl/sdk/math'
 import { UIController } from './uiController'
 import { KeyBoardUI } from '../uis/keyboardUI'
 import * as utils from '@dcl-sdk/utils'
+import { Dialogs } from '../npc_dialogs'
+
 export class GameController {
   mainInstance: MainInstace
   uiController: UIController
   keyBoardUI: KeyBoardUI
+  dialogs: Dialogs
   constructor() {
     this.mainInstance = new MainInstace(this)
     this.uiController = new UIController(this)
     this.keyBoardUI = new KeyBoardUI(this)
-    this.preloadScene()
-  }
-  preloadScene() {
+    this.dialogs = new Dialogs(this)
     this.keyBoardUI.isVisible = true
-    cleanUpScene()
-    this.mainInstance = new MainInstace(this)
     utils.timers.setTimeout(() => {
       movePlayerTo({
         newRelativePosition: Vector3.create(224.127, 69.7368, 124.0051),
         cameraTarget: Vector3.create(219.13, 70.73, 125.91)
       })
-    }, 5000);
-  }
+    }, 5000)
+}
 }
