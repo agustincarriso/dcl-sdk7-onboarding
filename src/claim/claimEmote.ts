56d98c659bbbc0cc47c1a278bbf1336dd20a4b29
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2024-09-25 09:12:29 -0300

    Fix: Remove close icon's from Captcha UIS

--- src/claim/claimEmote.ts
+++ src/claim/claimEmote.ts
@@ -98,6 +98,7 @@ export class ClaimEmoteTokenRequest {
       yPosition: 0,
       onMouseDown: () => {}
     })
+    this.captchaUI.closeIcon.hide()
     this.createRetryUI()
     this.createInProgressUI()
     this.claimInProgress.hide()
